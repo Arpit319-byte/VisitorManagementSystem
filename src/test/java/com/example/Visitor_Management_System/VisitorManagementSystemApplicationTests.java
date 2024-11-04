783df085b54a0a8c3774e81ab318a215ae77a696
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-04 22:52:34 +0530

    first commit

---
+++ src/test/java/com/example/Visitor_Management_System/VisitorManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Visitor_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
