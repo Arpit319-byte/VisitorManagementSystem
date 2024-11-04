0f26e73594df9b717b2e224435a85e5e1e0c8716
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-04 23:37:27 +0530

    Added the attributes for the Visitor class

--- src/main/java/com/example/Visitor_Management_System/Entity/Visitor.java
+++ src/main/java/com/example/Visitor_Management_System/Entity/Visitor.java
@@ -1,2 +1,46 @@
-package com.example.Visitor_Management_System.Entity;public class Visitor {
+package com.example.Visitor_Management_System.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Visitor {
+
+    @Id
+    @Column(nullable=false,updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long visitorId;
+
+    @Column(nullable = false)
+    private String visitorName;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false,unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false,unique = true)
+    private String idProof;
+
+    @Column(nullable = false)
+    private String idProofNumber;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime UpdatedAt;
+
 }
