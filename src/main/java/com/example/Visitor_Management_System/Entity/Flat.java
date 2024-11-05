4fd4e070eac980bd6f5e35b0d436799c82978f41
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-05 12:10:46 +0530

    Added the attributes for Flat entity

--- src/main/java/com/example/Visitor_Management_System/Entity/Flat.java
+++ src/main/java/com/example/Visitor_Management_System/Entity/Flat.java
@@ -1,4 +1,37 @@
 package com.example.Visitor_Management_System.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Flat {
+
+    @Id
+    @Column(nullable = false,updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long flatId;
+
+    @Column(nullable = false,updatable = false,unique = true)
+    private String flatNumber;
+
+    @OneToOne
+    @JoinColumn(name = "addressId")
+    private Address address;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 }
