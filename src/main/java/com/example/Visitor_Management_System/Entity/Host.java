a2d68ca61be587ebd1362cf52b9217adb945add7
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-05 14:34:56 +0530

    Corrected the datatype for hostname

--- src/main/java/com/example/Visitor_Management_System/Entity/Host.java
+++ src/main/java/com/example/Visitor_Management_System/Entity/Host.java
@@ -25,7 +25,7 @@ public class Host {
     private Long hostId;
 
     @Column(nullable = false)
-    private Long hostName;
+    private String hostName;
 
     @Column(nullable = false,unique = true)
     private String hostEmail;
