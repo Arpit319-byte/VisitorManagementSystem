34a0f805d4d9b239518bae9bcde453ced551e1b1
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-05 10:32:53 +0530

    Added the attributes for the Visit class

--- src/main/java/com/example/Visitor_Management_System/Entity/Visit.java
+++ src/main/java/com/example/Visitor_Management_System/Entity/Visit.java
@@ -1,2 +1,48 @@
-package com.example.Visitor_Management_System.Entity;public class Visit {
+package com.example.Visitor_Management_System.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.hibernate.boot.jaxb.internal.stax.LocalSchemaLocator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Visit {
+
+    @Id
+    @Column(nullable = false,updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long visitId;
+
+    private Long visitorId;
+
+    private Long hostId;
+
+    @Column(nullable = false)
+    private String purposeOfVisit;
+
+    @Column(nullable = false)
+    private LocalDateTime checkInTime;
+
+    @Column(nullable = false)
+    private LocalDateTime checkOutTime;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
 }
