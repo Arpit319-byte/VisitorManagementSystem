6ffc559dc26dbd5da476936eaa98fbb03d6956a3
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-05 14:44:46 +0530

    Added the HostDTO attributes and Validation Dependency in pom

--- src/main/java/com/example/Visitor_Management_System/DTO/HostDTO.java
+++ src/main/java/com/example/Visitor_Management_System/DTO/HostDTO.java
@@ -1,5 +1,8 @@
 package com.example.Visitor_Management_System.DTO;
 
+import com.example.Visitor_Management_System.Entity.Flat;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +10,22 @@ import lombok.Setter;
 @Setter
 public class HostDTO {
 
-    priv
+    private Long id;
+
+    @NotNull
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Size(max = 255)
+    private String number;
+
+    @NotNull
+    @Size(max = 255)
+    private String email;
+
+    @NotNull
+    private HostStatus status;
+
+    private Flat flat;
 }
