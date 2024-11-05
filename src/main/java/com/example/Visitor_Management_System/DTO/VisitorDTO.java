2ddc3a8f0ce0edfa33198011d717bbd1a3e24e9e
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-05 15:03:29 +0530

    Added the attributes for the VisitorDTO class

---
+++ src/main/java/com/example/Visitor_Management_System/DTO/VisitorDTO.java
@@ -0,0 +1,31 @@
+package com.example.Visitor_Management_System.DTO;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VisitorDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size(max=255)
+    private String name;
+
+    @NotNull
+    @Size(max=255)
+    private String number;
+
+    @NotNull
+    @Size(max=255)
+    private String idProof;
+
+    @NotNull
+    @Size(max=255)
+    private String idProofNumber;
+
+
+}
