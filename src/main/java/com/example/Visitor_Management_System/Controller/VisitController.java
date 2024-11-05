705695784f7ccb4164af281c671717ec30b0e81f
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-05 22:43:34 +0530

    Added the CRUD APIs for the VisitorController class

--- src/main/java/com/example/Visitor_Management_System/Controller/VisitController.java
+++ src/main/java/com/example/Visitor_Management_System/Controller/VisitController.java
@@ -1,4 +1,56 @@
 package com.example.Visitor_Management_System.Controller;
 
+
+import com.example.Visitor_Management_System.DTO.VisitorDTO;
+import com.example.Visitor_Management_System.Entity.Visit;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/visitor", produces = MediaType.APPLICATION_JSON_VALUE)
 public class VisitController {
+
+    private final VisitService visitService;
+
+    public VisitController(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<VisitorDTO>> getALLVisitors() {
+        return ResponseEntity.ok(visitService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VisitorDTO> getVisitor(@PathVariable(name = "id") Long id) {
+
+        VisitorDTO visitor = visitService.getById(id);
+
+        if (visitor != null)
+            return ResponseEntity.ok(visitor);
+        else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> addVisitor(@RequestBody @Valid final VisitorDTO visitorDTO) {
+
+        final Long createdId = visitService.createVisitor(visitorDTO);
+        return new ResponseEntity<>(createdId, HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVisitorById(@PathVariable(name = "id") @Valid Long id){
+         visitService.deleteById(id);
+         return ResponseEntity.noContent().build();
+    }
 }
