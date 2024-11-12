4361096b565eadd1e7a41bb83450ea8a96ce6e82
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-12 09:06:02 +0530

    Added the CRUD APIs method  for the VisitorController class

--- src/main/java/com/example/Visitor_Management_System/Controller/VisitorController.java
+++ src/main/java/com/example/Visitor_Management_System/Controller/VisitorController.java
@@ -1,4 +1,64 @@
 package com.example.Visitor_Management_System.Controller;
 
+import com.example.Visitor_Management_System.DTO.VisitorDTO;
+import com.example.Visitor_Management_System.Service.VisitorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/visitor")
 public class VisitorController {
+
+    private final VisitorService visitorService;
+
+    @Autowired
+    public VisitorController(VisitorService visitorService) {
+        this.visitorService = visitorService;
+    }
+
+    @GetMapping
+    private ResponseEntity<List<VisitorDTO>> getVisitors(){
+        return ResponseEntity.ok(visitorService.findAllVisitor());
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<VisitorDTO> getVisitor(@PathVariable(name = "id") Long id){
+        VisitorDTO visitorDTO = visitorService.getVisitorById(id);
+
+        if(visitorDTO != null){
+            return ResponseEntity.ok(visitorDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    private ResponseEntity<Long> addVisitor(@RequestBody @Valid VisitorDTO visitorDTO){
+        return ResponseEntity.ok(visitorService.addVisitor(visitorDTO));
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<VisitorDTO> updateVisitor(@PathVariable(name = "id") Long id,@RequestBody @Valid VisitorDTO visitorDTO){
+        VisitorDTO updatedVisitor = visitorService.updateVisitor(visitorDTO);
+
+        if(updatedVisitor != null){
+            return ResponseEntity.ok(updatedVisitor);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Void> deleteVisitor(@PathVariable(name = "id") Long id){
+        VisitorDTO visitorDTO = visitorService.getVisitorById(id);
+
+        if(visitorDTO != null){
+            visitorService.deleteVisitor(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/example/Visitor_Management_System/Service/VisitorService.java
+++ src/main/java/com/example/Visitor_Management_System/Service/VisitorService.java
@@ -1,6 +1,23 @@
 package com.example.Visitor_Management_System.Service;
+import com.example.Visitor_Management_System.DTO.VisitorDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VisitorService {
+    public List<VisitorDTO> findAllVisitor() {
+    }
+
+    public VisitorDTO getVisitorById(Long id) {
+    }
+
+    public Long addVisitor(VisitorDTO visitorDTO) {
+    }
+
+    public VisitorDTO updateVisitor(VisitorDTO visitorDTO) {
+    }
+
+    public void deleteVisitor(Long id) {
+    }
 }
