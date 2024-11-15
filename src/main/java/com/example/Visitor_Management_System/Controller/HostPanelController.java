734f2a74037f4a01d8b0689882260d2d245daa26
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-15 19:27:31 +0530

    Added the CRUD API'S for the HostPanelController class

--- src/main/java/com/example/Visitor_Management_System/Controller/HostPanelController.java
+++ src/main/java/com/example/Visitor_Management_System/Controller/HostPanelController.java
@@ -1,4 +1,37 @@
 package com.example.Visitor_Management_System.Controller;
 
+import com.example.Visitor_Management_System.DTO.VisitDTO;
+import com.example.Visitor_Management_System.Service.VisitService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/host")
 public class HostPanelController {
+
+    public final VisitService visitService;
+
+    @Autowired
+    public HostPanelController(VisitService visitService){
+        this.visitService=visitService;
+    }
+
+    @GetMapping("/allPendingVisits")
+    public ResponseEntity<List<VisitDTO>> getAllPendingVisits(){
+
+    }
+
+    @PutMapping("/approveVisit/{visitId}")
+    public ResponseEntity<Void> approveVisit(@PathVariable(name ="visitId") Long id){
+        return ResponseEntity.ok(visitService.approveVisit(id));
+    }
+
+    @PutMapping("/rejectVisit/{visitId}")
+    public ResponseEntity<Void> rejectVisit(@PathVariable(name ="visitId") Long id){
+        return ResponseEntity.ok(visitService.rejectVisit(id));
+    }
 }
