e9a570d30c72a7450446310717aa9b955b126dbe
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-16 11:44:10 +0530

    Added the attributes for the AddressDTO class

--- src/main/java/com/example/Visitor_Management_System/DTO/AddressDTO.java
+++ src/main/java/com/example/Visitor_Management_System/DTO/AddressDTO.java
@@ -1,4 +1,23 @@
 package com.example.Visitor_Management_System.DTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class AddressDTO {
+
+    @NotNull
+    @Size(max=255)
+    private String line1;
+
+    @NotNull
+    @Size(max=255)
+    private String line2;
+
+    @NotNull
+    @Size(max=255)
+    private String city;
+
+    @NotNull
+    @Size(max=255)
+    private String pinCode;
 }
