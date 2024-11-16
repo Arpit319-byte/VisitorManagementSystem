e22efcbac7a8e21a1a57901d7cbf8031073ab3c6
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-16 12:16:49 +0530

    AddressRepository class extends JpaRepository

--- src/main/java/com/example/Visitor_Management_System/Repository/AddressRepository.java
+++ src/main/java/com/example/Visitor_Management_System/Repository/AddressRepository.java
@@ -1,4 +1,7 @@
 package com.example.Visitor_Management_System.Repository;
 
-public interface AddressRepository {
+import com.example.Visitor_Management_System.Entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
 }
